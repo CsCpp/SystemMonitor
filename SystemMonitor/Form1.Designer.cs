37fa842a65070944636fe3485a1bcb0f866adea7
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-05-30 08:28:49 +0300

    хз

--- SystemMonitor/Form1.Designer.cs
+++ SystemMonitor/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
@@ -160,22 +160,22 @@
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(257, 239);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series3.Legend = "Legend1";
-            series3.Name = "ЦП";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.Name = "ОЗУ";
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "ЦП";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "ОЗУ";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(400, 188);
             this.chart1.TabIndex = 12;
             this.chart1.Text = "chart1";
@@ -190,6 +190,10 @@
             // 
             this.performanceRAM.CategoryName = "Memory";
             this.performanceRAM.CounterName = "%Commited Bytes In Use";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
--- SystemMonitor/Form1.cs
+++ SystemMonitor/Form1.cs
@@ -26,10 +26,18 @@ namespace SystemMonitor
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MEMORYSTATUSEX mEMORYSTATUSEX = new MEMORYSTATUSEX();
+            if (GlobalMemoryStatusEx(mEMORYSTATUSEX))
+            {
+                installedMemory = mEMORYSTATUSEX.ullTotalPhys;
 
+            }
+            metroLabel10.Text = Convert.ToString(installedMemory / 1000000000) + " Гб";
+            timer1.Interval = 1000;
+            timer1.Start();
         }
 
-        [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Auto)]
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
         private class MEMORYSTATUSEX
         {
 
@@ -45,8 +53,30 @@ namespace SystemMonitor
 
             public MEMORYSTATUSEX()
             {
-                this.dwLength=(uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+                this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
             }
+
+        }
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cpu = performanceCPU.NextValue();
+            ram = performanceRAM.NextValue();
+
+            metroProgressBar1.Value = (int)cpu;
+            metroProgressBar2.Value = (int)ram;
+
+            metroLabel2.Text = Convert.ToString(Math.Round(cpu, 1)) + " %";
+            metroLabel3.Text = Convert.ToString(Math.Round(ram, 1)) + " %";
+
+            metroLabel6.Text = Convert.ToString(Math.Round((ram / 100 * installedMemory) / 1000000000, 1)) + " Гб";
+            metroLabel8.Text = Convert.ToString(Math.Round((installedMemory - ram / 100 * installedMemory) / 1000000000, 1)) + " Гб";
+
+            chart1.Series["ЦП"].Points.AddY(cpu);
+            chart1.Series["ОЗУ"].Points.AddY(ram);
 
         }
     }
